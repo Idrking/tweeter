4a6bb5ec5461dc53485f6b7e5f38025cf53b87f5
Isaac King <idrkings@gmail.com>
2021-01-27 22:01:05 +0000

    Add renderTweets function and removed placeholder tweet in index.html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,32 +5,48 @@ $(document).ready(function() {
   
 
   //Helper functions for createTweetElement from /tweetAssembler.js  
-  const createTweetElement = (tweetData) => {
+  const createTweetElement = tweetData => {
     const $tweet = $(`<article class="tweet"></article`);
     $tweet.append(createHeader(tweetData)).append(createBody(tweetData)).append(createFooter(tweetData));
     return $tweet
   };
 
+  const renderTweets = tweetsToRender => {
+    const $tweetContainer = $('#tweet-feed');
+    for (let i = tweetsToRender.length - 1; i >= 0; i--) {
+      $tweetContainer.prepend(createTweetElement(tweetsToRender[i]));
+    }
+  };
 
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-   "user": {
-     "name": "Newton",
-     "avatars": "https://i.imgur.com/73hZDYK.png",
-       "handle": "@SirIsaac"
-     },
-   "content": {
-       "text": "If I have seen further it is by standing on the shoulders of giants"
-     },
-   "created_at": 1461116232227
-  }
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
   
-  const $tweet = createTweetElement(tweetData);
-  
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweet-feed').append($tweet); 
+  renderTweets(data);
 
 });
 
