6d32bd41efec691a4e0ee49b5546492e953127e8
Isaac King <idrkings@gmail.com>
2021-01-27 00:19:15 +0000

    Add styling for tweet-feed

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 
   $('#tweet-text').on('input', function () {
-    let counterNum = 140 - $(this).val().length;
+    const MAXTWEETLENGTH = 140;
+    const counterNum = MAXTWEETLENGTH - $(this).val().length;
     const counter = $(this).siblings().children('output');
     counter.html(counterNum);
     if (counterNum < 0) {
